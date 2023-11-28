d76ebec60d7b31e06d1da7af3d4ffe643e817bd6
AlexThe86 <mr_axel_peter@web.de>
2023-11-28 23:56:50 +0100

    commit alex

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -24,16 +26,27 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            var selectedFragment: Fragment? = null
-
+            val navController = findNavController(R.id.navHostFragment)
             when (item.itemId) {
-                R.id.homeFragment -> selectedFragment = HomeFragment()
-                R.id.searchFragment -> selectedFragment = SearchFragment()
-                R.id.cartFragment -> selectedFragment = CartFragment()
+                R.id.homeFragment -> {
+                    if (navController.currentDestination?.id != R.id.homeFragment) {
+                        navController.navigate(R.id.homeFragment)
+                    }
+                }
+                R.id.searchFragment -> {
+                    if (navController.currentDestination?.id != R.id.searchFragment) {
+                        navController.navigate(R.id.searchFragment)
+                    }
+                }
+                R.id.cartFragment -> {
+                    if (navController.currentDestination?.id != R.id.cartFragment) {
+                        navController.navigate(R.id.cartFragment)
+                    }
+                }
+                else -> {
+                    Log.d("activityMainBottomNav","Error in BottomNavigation")
+                }
             }
-
-            navController.navigate(selectedFragment!!.id)
-
             true
         }
     }
